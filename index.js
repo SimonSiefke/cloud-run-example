77296cd25092ea6681283ca43408c68d9ddad606
Simon Siefke <simon.siefke@gmail.com>
2020-12-05 22:16:57 +0100

    update

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 app.get('/', (req, res) => {
   const date = new Date().toString()
   res.set('Cache-Control', 'public, max-age=31536000')
-  res.send(`server time is ${date}`)
+  // res.send(`server time is ${date}`)
+  res.send('hello world')
 })
 
 const port = process.env.PORT || 8080
