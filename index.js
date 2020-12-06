0005b6072608d9612b97258206102c6e66f1230a
Simon Siefke <simon.siefke@gmail.com>
2020-12-06 11:45:24 +0100

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/', (req, res) => {
   const date = new Date().toString()
   res.set('Cache-Control', 'public, max-age=31536000')
-  res.send('hello from express')
+  res.send('hello world')
 })
 
 app.listen(8080, () => console.log(`listening on port 8080`))
